dd0e6fbea83ae627e4dc3b1c3c8c92d957167343
xufeifan <xufeifan1992@126.com>
2019-04-17 22:21:10 +0800

    xufeifan

--- src/main/java/com/array/ArrayQueue.java
+++ src/main/java/com/array/ArrayQueue.java
@@ -4,5 +4,58 @@ package com.array;
  * Created by xufei
  * 2019/4/17
  */
-public class ArrayQueue {
+public class ArrayQueue<E> implements Queue<E> {
+
+    private Array<E> array;
+
+    public ArrayQueue(int capacity){
+        array = new Array<>(capacity);
+    }
+
+    public ArrayQueue(){
+        array = new Array<>();
+    }
+
+    @Override
+    public int getSize() {
+        return array.getSize();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return array.isEmpty();
+    }
+
+    @Override
+    public void enqueue(E e) {
+        array.addLast(e);
+    }
+
+    @Override
+    public E dequeue() {
+        return array.removeFirst();
+    }
+
+    @Override
+    public E getFront() {
+        return array.getFirst();
+    }
+
+    public int getCapacity(){
+        return array.getCapacity();
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Queue：");
+        stringBuilder.append("front [");
+        for (int i = 0; i < array.getSize(); i++) {
+            if(i != array.getSize() - 1){
+                stringBuilder.append(", ");
+            }
+        }
+        stringBuilder.append("] tail");
+        return stringBuilder.toString();
+    }
 }
--- src/main/java/com/array/Queue.java
+++ src/main/java/com/array/Queue.java
@@ -4,5 +4,14 @@ package com.array;
  * Created by xufei
  * 2019/4/17
  */
-public interface Queue {
+public interface Queue<E> {
+    int getSize();
+
+    boolean isEmpty();
+
+    void enqueue(E e);
+
+    E dequeue();
+
+    E getFront();
 }
