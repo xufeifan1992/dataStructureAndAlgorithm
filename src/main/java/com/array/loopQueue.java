737d8ee0006dd6c07a46d76477f79be306a6ba51
xufeifan <xufeifan1992@126.com>
2019-04-18 16:51:11 +0800

    增加循环队列代码实现

---
+++ src/main/java/com/array/loopQueue.java
@@ -0,0 +1,83 @@
+package com.array;
+
+/**
+ * Created by xufei
+ * 2019/4/18
+ */
+public class loopQueue<E> implements Queue<E> {
+
+    private E[] data;
+    private int front, tail;
+    private int size;
+
+    public loopQueue(int capacity) {
+        data = (E[]) new Object[capacity + 1];
+        front = 0;
+        tail = 0;
+        size = 0;
+    }
+
+    public loopQueue() {
+        this(10);
+    }
+
+    public int getCapacity() {
+        return data.length - 1;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return front == tail;
+    }
+
+    @Override
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public void enqueue(E e) {
+        if ((tail + 1) % data.length == front) {
+            resize(getCapacity() * 2);
+            data[tail] = e;
+            tail = (tail + 1) % data.length;
+            size++;
+        }
+    }
+
+    @Override
+    public E dequeue() {
+        if (isEmpty()) {
+            throw new IllegalArgumentException("队列为空");
+        }
+        E ret = data[front];
+        data[front] = null;
+        front = (front + 1) % data.length;
+        size--;
+        if (size == getCapacity() / 4 && getCapacity() / 2 != 0) {
+            resize(getCapacity() / 2);
+        }
+
+        return ret;
+
+    }
+
+    @Override
+    public E getFront(){
+        if(isEmpty()){
+            throw new IllegalArgumentException("队列为空");
+        }
+        return data[front];
+    }
+
+    private void resize(int newCapacity) {
+        E[] newData = (E[]) new Object[newCapacity + 1];
+        for (int i = 0; i < size; i++) {
+            newData[i] = data[(i + front) % data.length];
+        }
+        data = newData;
+        front = 0;
+        tail = size;
+    }
+
+}
