5a03aa9fdb0245ad104e713716834f4195a984ef
xufeifan <xufeifan1992@126.com>
2019-04-20 22:04:44 +0800

    提交leetCode题code

---
+++ src/main/java/com/exercises/ListNode.java
@@ -0,0 +1,14 @@
+package com.exercises;
+
+/**
+ * Created by xufei
+ * 2019/4/20
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
--- src/main/java/com/exercises/Solution.java
+++ src/main/java/com/exercises/Solution.java
@@ -10,6 +10,7 @@ import java.util.Stack;
  * 2019/4/17
  */
 public class Solution {
+    //括号匹配
     public boolean isValid(String s) {
         Stack stack = new Stack();
         char[] chars = s.toCharArray();
@@ -33,6 +34,33 @@ public class Solution {
         }
 
         return stack.isEmpty();
+    }
+
+    //删除链表中多个元素
+    public ListNode removeElements(ListNode head, int val) {
+
+        while (head != null && head.val == val) {
+            ListNode delNode = head;
+            head = head.next;
+            delNode.next = null;
+        }
+
+        if(head == null){
+            return head;
+        }
+
+        ListNode prev = head;
+        while(prev.next != null){
+            if(prev.next.val == val){
+                ListNode delNode = prev.next;
+                prev.next = delNode.next;
+                delNode.next = null;
+            }else{
+                prev = prev.next;
+            }
+        }
+        return head;
+
     }
 
     @Test
